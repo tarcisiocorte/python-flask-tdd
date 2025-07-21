b54f356086d190d829b097952cd99fd0de6ad3eb
Tarcisio Corte <TCorte@totalwine.com>
2025-07-21 07:59:13 -0300

    refactor: simplify test setup in TestSignUpController by introducing make_sut function

--- test_signup_controller.py
+++ test_signup_controller.py
@@ -3,69 +3,76 @@ from signup import SignUpController
 from protocols.http import HttpRequest
 
 
-class TestSignUpController(unittest.TestCase):
-    def setUp(self):
-        self.sut = SignUpController()
+def make_sut() -> SignUpController:
+    return SignUpController()
 
+
+class TestSignUpController(unittest.TestCase):
     def test_should_return_400_if_no_name_provided(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "email": "any_email@mail.com",
             "password": "any_password",
             "passwordConfirmation": "any_password"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 400)
         self.assertEqual(http_response.body, {"error": "Missing param: name"})
 
     def test_should_return_400_if_no_email_provided(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "name": "any_name",
             "password": "any_password",
             "passwordConfirmation": "any_password"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 400)
         self.assertEqual(http_response.body, {"error": "Missing param: email"})
 
     def test_should_return_400_if_no_password_provided(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "name": "any_name",
             "email": "any_email@mail.com",
             "passwordConfirmation": "any_password"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 400)
         self.assertEqual(http_response.body, {"error": "Missing param: password"})
 
     def test_should_return_400_if_no_password_confirmation_provided(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "name": "any_name",
             "email": "any_email@mail.com",
             "password": "any_password"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 400)
         self.assertEqual(http_response.body, {"error": "Missing param: passwordConfirmation"})
 
     def test_should_return_400_if_password_confirmation_fails(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "name": "any_name",
             "email": "any_email@mail.com",
             "password": "any_password",
             "passwordConfirmation": "invalid_confirmation"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 400)
         self.assertEqual(http_response.body, {"error": "Password confirmation does not match password"})
 
     def test_should_return_200_if_all_data_is_valid(self):
+        sut = make_sut()
         http_request = HttpRequest({
             "name": "any_name",
             "email": "any_email@mail.com",
             "password": "any_password",
             "passwordConfirmation": "any_password"
         })
-        http_response = self.sut.handle(http_request)
+        http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 200)
         self.assertEqual(http_response.body, {"message": "User signed up successfully"})
 
