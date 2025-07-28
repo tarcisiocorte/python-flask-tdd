c8a772a82656287b9fb7e7c8e4b34a4b0acfe81e
Tarcisio Corte <tarcisiocorte@gmail.com>
2025-07-28 08:06:12 -0300

    test: add unit test to verify AddAccount is called with correct values in SignUpController

--- test_signup_controller.py
+++ test_signup_controller.py
@@ -1,10 +1,26 @@
 import unittest
+from unittest.mock import Mock
 from signup import SignUpController
 from protocols.http import HttpRequest
+from domain.usecases.add_account import AddAccount, AddAccountModel
+from domain.models.account import AccountModel
+
+
+def make_add_account_stub() -> AddAccount:
+    class AddAccountStub(AddAccount):
+        def add(self, account: AddAccountModel) -> AccountModel:
+            return AccountModel(
+                id="valid_id",
+                name="valid_name",
+                email="valid_email@mail.com",
+                password="valid_password"
+            )
+    return AddAccountStub()
 
 
 def make_sut() -> SignUpController:
-    return SignUpController()
+    add_account_stub = make_add_account_stub()
+    return SignUpController(add_account_stub)
 
 
 class TestSignUpController(unittest.TestCase):
@@ -75,6 +91,28 @@ class TestSignUpController(unittest.TestCase):
         http_response = sut.handle(http_request)
         self.assertEqual(http_response.status_code, 200)
         self.assertEqual(http_response.body, {"message": "User signed up successfully"})
+
+    def test_should_call_add_account_with_correct_values(self):
+        add_account_spy = Mock(spec=AddAccount)
+        add_account_spy.add.return_value = AccountModel(
+            id="valid_id",
+            name="valid_name",
+            email="valid_email@mail.com",
+            password="valid_password"
+        )
+        sut = SignUpController(add_account_spy)
+        http_request = HttpRequest({
+            "name": "any_name",
+            "email": "any_email@mail.com",
+            "password": "any_password",
+            "passwordConfirmation": "any_password"
+        })
+        sut.handle(http_request)
+        add_account_spy.add.assert_called_once_with(AddAccountModel(
+            name="any_name",
+            email="any_email@mail.com",
+            password="any_password"
+        ))
 
 
 if __name__ == '__main__':
