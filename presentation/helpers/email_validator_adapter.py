952456b82efbd9d419969888425e5f6145d955c8
Tarcísio Corte <tarcisiocorte@gmail.com>
2025-09-10 15:36:40 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- presentation/helpers/email_validator_adapter.py
+++ presentation/helpers/email_validator_adapter.py
@@ -1,6 +1,11 @@
+from email_validator import validate_email, EmailNotValidError
 from protocols.email_validator import EmailValidator
 
 
 class EmailValidatorAdapter(EmailValidator):
     def is_valid(self, email: str) -> bool:
-        return False
+        try:
+            validate_email(email, check_deliverability=False)
+            return True
+        except EmailNotValidError:
+            return False
