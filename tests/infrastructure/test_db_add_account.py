656bbc7e33e52321a4a0c0af42a09bfe7827d40e
Tarcísio Corte <tarcisiocorte@gmail.com>
2025-10-06 14:34:47 -0300

    refactor: enhance test setup by introducing a factory method for DbAddAccount and EncrypterStub

--- tests/infrastructure/test_db_add_account.py
+++ tests/infrastructure/test_db_add_account.py
@@ -3,18 +3,34 @@ import asyncio
 from unittest.mock import AsyncMock, patch
 from infrastructure.db_add_account import DbAddAccount
 from domain.usecases.add_account import AddAccountModel
+from protocols.encrypter import Encrypter
+from typing import NamedTuple
 
 
-class EncrypterStub:
+class EncrypterStub(Encrypter):
     async def encrypt(self, value: str) -> str:
         return "hashed_password"
 
 
+class SutTypes(NamedTuple):
+    sut: DbAddAccount
+    encrypter_stub: EncrypterStub
+
+
+def make_sut() -> SutTypes:
+    encrypter_stub = EncrypterStub()
+    sut = DbAddAccount(encrypter_stub)
+    return SutTypes(
+        sut=sut,
+        encrypter_stub=encrypter_stub
+    )
+
+
 class TestDbAddAccount(unittest.TestCase):
     def test_should_call_encrypter_with_correct_password(self):
-
-        encrypter_stub = EncrypterStub()
-        sut = DbAddAccount(encrypter_stub)
+        sut_types = make_sut()
+        sut = sut_types.sut
+        encrypter_stub = sut_types.encrypter_stub
 
         with patch.object(encrypter_stub, 'encrypt', new_callable=AsyncMock) as encrypt_spy:
             account_data = AddAccountModel(
